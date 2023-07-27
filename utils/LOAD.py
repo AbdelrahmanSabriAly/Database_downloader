e7f38ab8e6ac2ac800e32cac917e285f00274339
Abdelrahman Sabri <abdelrahman.aly02@eng-st.cu.edu.eg>
2023-07-27 23:28:11 +0300

    syntax error in st.secrets()

--- utils/LOAD.py
+++ utils/LOAD.py
@@ -11,17 +11,17 @@ from tqdm import tqdm
 
 def create_keyfile_dict():
     variables_keys = {
-        "type": st.secrets("TYPE"),
-        "project_id": st.secrets("PROJECT_ID"),
-        "private_key_id": st.secrets("PRIVATE_KEY_ID"),
-        "private_key": st.secrets("PRIVATE_KEY"),
-        "client_email": st.secrets("CLIENT_EMAIL"),
-        "client_id": st.secrets("CLIENT_ID"),
-        "auth_uri": st.secrets("AUTH_URI"),
-        "token_uri": st.secrets("TOKEN_URI"),
-        "auth_provider_x509_cert_url": st.secrets("AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": st.secrets("CLIENT_X509_CERT_URL"),
-        "universe_domain" : st.secrets("UNIVERSE_DOMAIN")
+        "type": st.secrets["TYPE"],
+        "project_id": st.secrets["PROJECT_ID"],
+        "private_key_id": st.secrets["PRIVATE_KEY_ID"],
+        "private_key": st.secrets["PRIVATE_KEY"],
+        "client_email": st.secrets["CLIENT_EMAIL"],
+        "client_id": st.secrets["CLIENT_ID"],
+        "auth_uri": st.secrets["AUTH_URI"],
+        "token_uri": st.secrets["TOKEN_URI"],
+        "auth_provider_x509_cert_url": st.secrets["AUTH_PROVIDER_X509_CERT_URL"],
+        "client_x509_cert_url": st.secrets["CLIENT_X509_CERT_URL"],
+        "universe_domain" : st.secrets["UNIVERSE_DOMAIN"]
     }
     return variables_keys
 
